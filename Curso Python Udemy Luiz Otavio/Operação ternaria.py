038cf74b8304d27ec3d98b01f306ae860214d408
lucasdominguess <lucasdominguesofficial@gmail.com>
2023-07-04 10:17:31 -0300

    dia 04
    dia 04

---
+++ Curso Python Udemy Luiz Otavio/Operação ternaria.py
@@ -0,0 +1,5 @@
+valor = 2 #valor 2
+print ( "valor "if false else "outro valor" if false else "fim ")
+
+#Video 98 explicação 
+
---
+++ Curso Python Udemy Luiz Otavio/Validaçaõ cpf.py
@@ -0,0 +1,12 @@
+'''Cpf 41487305869'''
+
+#colete a soma dos 9 primeiros digitos do CPF 
+'''multiplicando cada u dos valores por uma contagem regressiva començando de 10
+
+
+ex :
+4  1 4  8  7   3  0  5  8
+10 9 8  7  6   5  4  3  2 
+40 9 32 56  42 15 0  15 16  '''
+
+print (1890 % 11)
